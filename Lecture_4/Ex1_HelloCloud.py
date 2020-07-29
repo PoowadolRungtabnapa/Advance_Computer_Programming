22e1fcae45a9146b52ccaa3a7bc470002efe2089
INE-01 <INE-01@Admins-iMac-5.local>
2020-07-29 18:43:40 +0700

    .

--- Lecture_4/Ex1_HelloCloud.py
+++ Lecture_4/Ex1_HelloCloud.py
@@ -1,18 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, Integer, String, Date
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://webadmin:XVBqxc19866@node1449-testdb.app.ruk-com.cloud:11025/testdb'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+class Comments(db.Model):
+    __tablename__ = 'comments'
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    comment = Column(String)
 
 @app.route('/')
 def index() :
-    return '<h1>Hello RUK-COM!</h1>'
+    result = Comments.query.all()
+    return render_template('index.html', result=result)
 
-@app.route('/home', methods=['GET','POST'])
-def home() :
-    links = ['https://ruk-com.in.th',
-             'https://www.google.com',
-             'https://www.python.org',
-             'https://www.kmutnb.ac.th']
-    return render_template('example.html', links=links)
+@app.route('/sign')
+def sign() :
+    return render_template('sign.html')
+
+@app.route('/process', methods=['POST'])
+def process() :
+    name = request.form['name']
+    comment = request.form['comment']
+    signature = Comments(name=name, comment=comment)
+    db.session.add(signature)
+    db.session.commit()
+    return redirect(url_for('index'))
 
 if __name__ == '__main__' :
     app.debug = True
